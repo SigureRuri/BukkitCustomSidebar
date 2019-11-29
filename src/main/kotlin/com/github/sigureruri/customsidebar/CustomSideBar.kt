8adcfcb7abe798055e5e54ec3fff2ce52716ab63
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2019-11-29 23:53:26 +0900

    Create base

---
+++ src/main/kotlin/com/github/sigureruri/customsidebar/CustomSideBar.kt
@@ -0,0 +1,14 @@
+package com.github.sigureruri.customsidebar
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CustomSideBar : JavaPlugin() {
+
+    // PLUGIN startup logic
+    override fun onEnable() {
+    }
+
+    // PLUGIN shutdown logic
+    override fun onDisable() {
+    }
+}
