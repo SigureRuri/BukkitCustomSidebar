f55fbce069b9d6f5e3bfed22a1f602e7f186c3ac
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2019-12-05 18:56:41 +0900

    Add Sidebar.kt

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
 # CustomSideBar
+仮想エンティティを使用したScoreboardSidebarをプレイヤーに表示が可能です。  
+時間経過によるTitleの変更や、 Sidebarの更新が可能です。
+
+## 環境
+- Java 8
+- Kotlin 1.3.60
+- Spigot 1.14.4
+
+## Sample
+Java:
+```java
+public class SampleSidebarJava extends Sidebar {
+
+    public SampleSidebarJava(JavaPlugin plugin) {
+        super(plugin, "samplesidebar");
+    }
+
+    @NotNull
+    @Override
+    public List<Pair<String, Integer>> getTitle(@NotNull Player player) {
+        return new ArrayList<>(
+                Arrays.asList(
+                        new Pair<>("§b§lSample", 5),
+                        new Pair<>("§3§lSample", 5),
+                        new Pair<>("§b§lSample", 5),
+                        new Pair<>("§3§lSample", 5),
+                        new Pair<>("§b§lS§3§lample", 1),
+                        new Pair<>("§3§lS§b§la§3§lmple", 1),
+                        new Pair<>("§3§lSa§b§lm§3§lple", 1),
+                        new Pair<>("§3§lSam§b§lp§3§lle", 1),
+                        new Pair<>("§3§lSamp§b§ll§3§le", 1)
+                )
+        );
+    }
+
+    @NotNull
+    @Override
+    public List<Pair<String, Integer>> getContents(@NotNull Player player) {
+        return new ArrayList<>(
+                Arrays.asList(
+                        new Pair<>(" ", 15),
+                        new Pair<>("§c§lHealth§f: §c§l" + Math.round(player.getHealth()) + "§7/§c§l" + Math.round(player.getHealthScale()), 14),
+                        new Pair<>("  ", 13),
+                        new Pair<>("§6§lOnline Players§f: §e§l" + Bukkit.getOnlinePlayers().size(), 12),
+                        new Pair<>("   ", 11)
+                )
+        );
+    }
+}
+```
+Kotlin:
+```kotlin
+class SampleSidebarKotlin(plugin: JavaPlugin) : Sidebar(plugin, "samplesidebar") {
+    override fun getTitle(player: Player): List<Pair<String, Int>> {
+        return listOf(
+                Pair("§b§lSample", 5),
+                Pair("§3§lSample", 5),
+                Pair("§b§lSample", 5),
+                Pair("§3§lSample", 5),
+                Pair("§b§lS§3§lample", 1),
+                Pair("§3§lS§b§la§3§lmple", 1),
+                Pair("§3§lSa§b§lm§3§lple", 1),
+                Pair("§3§lSam§b§lp§3§lle", 1),
+                Pair("§3§lSamp§b§ll§3§le", 1)
+        )
+    }
+
+    override fun getContents(player: Player): List<Pair<String, Int>> {
+        return listOf(
+                Pair(" ", 15),
+                Pair("§c§lHealth§f: §c§l" + player.health.roundToInt() + "§7/§c§l" + player.healthScale.roundToInt(), 14),
+                Pair("  ", 13),
+                Pair("§6§lOnline Players§f: §e§l" + Bukkit.getOnlinePlayers().size, 12),
+                Pair("   ", 11)
+        )
+    }
+}
+```
+
---
+++ src/main/kotlin/com/github/sigureruri/customsidebar/Sidebar.kt
@@ -0,0 +1,139 @@
+package com.github.sigureruri.customsidebar
+
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
+import org.bukkit.scoreboard.DisplaySlot
+
+/**
+ * ScoreboardSidebarを利用し、PlayerにSideBarを表示する。
+ * 仮想エンティティの表示名を利用するため、仮想エンティティのScoreを変更するような使用法は想定していない。
+ * PlayerのScoreboardをMainScoreboardから変更するため、他のスコアボードを使用するシステムと競合する恐れがある。
+ *
+ * @author SigureRuri
+ *
+ * @param plugin plugin
+ * @param objectiveName ScoreboardObjectiveの名称。
+ *     表示名には影響しない。
+ */
+abstract class Sidebar(
+        val plugin: JavaPlugin,
+        val objectiveName: String
+) {
+
+    /**
+     * SidebarのTitleを取得する。
+     * 一種類のみにする場合はsecondは適当でよい。
+     *
+     * @return Titleの一覧。
+     *     first -> 表示するTitle
+     *     second -> 次を表示するまでのTick
+     */
+    abstract fun getTitle(
+            player: Player
+    ): List<Pair<String, Int>>
+
+    /**
+     * SidebarのContentsを取得する。
+     * Sidebarには15列までのみ表示が可能です。
+     * 同一の文字列は指定できません。
+     *
+     * @return first -> secondにて指定した仮想エンティティに割り当てるScoreの値
+     *     second -> 仮想エンティティの名称
+     */
+    abstract fun getContents(
+            player: Player
+    ): List<Pair<String, Int>>
+
+    /**
+     * SidebarのContentsを更新する
+     *
+     * @param player Sidebarを更新するプレイヤー
+     */
+    fun update(
+            player: Player
+    ) {
+        val scoreboard = player.scoreboard
+        val objective = scoreboard.getObjective(DisplaySlot.SIDEBAR) ?: return
+        if (objective.name != objectiveName || objective.criteria != CRITERIA_DUMMY) return
+
+        var update = false
+        getContents(player).forEach {
+            if (!objective.getScore(it.first).isScoreSet) update = true
+        }
+        if (!update) return
+
+        scoreboard.entries.forEach {
+            scoreboard.resetScores(it)
+        }
+        getContents(player).forEach { (content, score) ->
+            objective.getScore(content).score = score
+        }
+    }
+
+    /**
+     * Sidebarを表示する
+     *
+     * @param player Sidebarを表示するプレイヤー
+     * @param updateIntervalTicks Sidebarを更新する間隔。[-1L]にすると更新されない。
+     */
+    fun show(
+            player: Player,
+            updateIntervalTicks: Long = 1L
+    ) {
+        val title = if (getTitle(player).isNotEmpty()) getTitle(player).first().first else TITLE_EMPTY
+        val newScoreboard = SCOREBOARD_MANAGER.newScoreboard
+        val objective = newScoreboard.registerNewObjective(objectiveName, CRITERIA_DUMMY, title).apply {
+            displaySlot = DisplaySlot.SIDEBAR
+        }
+        getContents(player).forEach { (content, score) ->
+            objective.getScore(content).score = score
+        }
+        player.scoreboard = newScoreboard
+
+        if (updateIntervalTicks >= 0) {
+            object : BukkitRunnable() {
+                override fun run() {
+                    update(player)
+                }
+            }.runTaskTimer(plugin, updateIntervalTicks, updateIntervalTicks)
+        }
+
+        if (getTitle(player).size > 1) {
+            val titleList = getTitle(player)
+            object : BukkitRunnable() {
+                var stage = 0
+                var count = 0
+                override fun run() {
+                    if (titleList[stage].second <= count) {
+                        count = 0
+                        stage = if (titleList.lastIndex <= stage) 0 else stage + 1
+                        objective.displayName = titleList[stage].first
+                    } else {
+                        count++
+                    }
+                }
+            }.runTaskTimer(plugin, 0L, 1L)
+        }
+    }
+
+    /**
+     * Sidebarを非表示にする
+     * 対象のPlayerのScoreboardをMainScoreboardに変更する
+     *
+     * @param player Sidebarを非表示にするプレイヤー
+     */
+    fun hide(
+            player: Player
+    ) {
+        player.scoreboard = SCOREBOARD_MANAGER.mainScoreboard
+    }
+
+    companion object {
+        private val SCOREBOARD_MANAGER = Bukkit.getScoreboardManager()!!
+        private val CRITERIA_DUMMY = "dummy"
+        private val TITLE_EMPTY = "${ChatColor.GRAY}${ChatColor.BOLD}Title is empty"
+    }
+}
